24967b66d972f9e05283adea1d4006d35776d0a4
Lamby777 <sheepareawesome777@gmail.com>
2019-12-17 16:06:18 +0000

    clear function

--- main.py
+++ main.py
@@ -3,5 +3,19 @@
 #  https://github.com/Lamby777/SlurPy  #
 ########################################
 
+# Imports
+from termcolor import colored;
+import os.system as cmd, os.name as OSName
+#import a, b, c as d
+
+# Functions
+
+def cls(): 
+    if OSName == 'nt': 
+        cmd('cls')
+    else: 
+        cmd('clear')
+
+# Execute Code
 with open("code.jc") as f:
 	pass
