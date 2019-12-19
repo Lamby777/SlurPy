4528c1925f25c0f6db9043c5d7489f8382afe2cd
Lamby777 <sheepareawesome777@gmail.com>
2019-12-19 15:21:30 +0000

    robux

--- main.py
+++ main.py
@@ -10,28 +10,27 @@ import fn, math
 
 # Constants
 ops = [
-	["ps", lambda x, y: x+y],
-	["sb", lambda x, y: x-y],
-	["mt", lambda x, y: x*y],
-	["dv", lambda x, y: x/y],
-	["pw", lambda x, y: x**y],
-	["sq", lambda x: math.sqrt(x)],
+	{"ps": lambda x, y: x+y},
+	{"sb": lambda x, y: x-y},
+	{"mt": lambda x, y: x*y},
+	{"dv": lambda x, y: x/y},
+	{"pw": lambda x, y: x**y},
+	{"sq": lambda x: math.sqrt(x)},
 ]
-keywords = [i[0] for i in ops]
+keywords = [i for i in ops]
 
 cmds = [
-	["echo", lambda x: print(x)],
-	["clear", fn.cls],
-	["script", fn.runJuice],
+	{"echo": [lambda x: print(x), "Prints to terminal"]},
+	{"clear": [fn.cls, "Clears terminal"]},
+	{"script": [fn.runJuice, "Runs file"]},
 ]
-cmdk = [i[0] for i in cmds]
+cmdk = [i for i in cmds]
 
+print(colored("Welcome to SlurpTerminal"))
 while True:
-	print(colored("Welcome to SlurpTerminal"))
+	print(">", end=" ")
 	cmd = input().lower().strip()
-	if cmd in cmdk:
-		while True:
-			print("> ", end="")
-			shellJuice = input()
-			if not shellJuice in keywords:
-				raise oof["InvalidCmd"]
+	if not cmd in cmds:
+		fn.out("Invalid Command! Use \"help\"", "red")
+	else:
+		cmds[cmd]
