4e4adf59c05616308ac20389f42ad45a5269311b
Lamby777 <sheepareawesome777@gmail.com>
2019-12-19 17:54:36 +0000

    cmds now run, and help command works

--- main.py
+++ main.py
@@ -20,18 +20,23 @@ ops = {
 keywords = [i for i in ops]
 
 cmds = {
+	"help": [lambda: (
+		print("\n")
+		[print(i + "\n|> " + cmds[i][1]) for i in cmds]
+		print("\n")
+	), "Lists all commands"],
 	"echo": [lambda x: print(x), "Prints to terminal"],
 	"clear": [fn.cls, "Clears terminal"],
 	"script": [fn.runJuice, "Runs file"],
 }
-cmdk = [i for i in cmds]
 
 print(colored("Welcome to SlurpTerminal"))
 while True:
 	print(">", end=" ")
 	cmdfull = input().lower().strip().split(" ")
 	cmd = cmdfull[0]
-	if not cmd in cmdk:
+	if not cmd in cmds:
 		fn.out("Invalid Command! Use \"help\"", "red")
 	else:
 		cmdargs = " ".join(cmdfull[1:])
+		cmds[cmd][0]()
