7eeca962d19f4b03e6ac151f16e92c9462934589
Lamby777 <sheepareawesome777@gmail.com>
2019-12-19 14:43:24 +0000

    error and shell

--- main.py
+++ main.py
@@ -7,13 +7,27 @@
 from termcolor import colored
 from inspect import signature
 from os import system as cmd, name as OS
+import math
 
 # Constants
 ops = [
-	[]
+	["ps", lambda x, y: x+y],
+	["sb", lambda x, y: x-y],
+	["mt", lambda x, y: x*y],
+	["dv", lambda x, y: x/y],
+	["pw", lambda x, y: x**y],
+	["sq", lambda x: math.sqrt(x)],
 ]
 
 keywords = [i[0] for i in ops]
+
+class MetaException(Exception):
+	def __init__(self, name):
+		self.name = name
+
+oofs = {
+	"InvalidCmd": MetaException("Invalid Command!")
+}
 
 # Functions
 
@@ -36,7 +50,16 @@ def interpret(code):
 def out(msg, color):
 	print(colored(msg, color))
 
-# Execute Code
-with open("code.jc") as f:
-	code = f.read()
-	interpret(code)
+while True:
+	print("Type S for shell or R to run code.jc")
+	mode = input().lower().strip()
+	if mode == "s":
+		while True:
+			shellJuice = input()
+			if not shellJuice in keywords:
+				raise oofs.InvalidCmd
+	elif mode == "r":
+		# Execute Code
+		with open("code.jc") as f:
+			code = f.read()
+			interpret(code)
