9576e1d254872c197260ae912d78c77caccd6478
Lamby777 <sheepareawesome777@gmail.com>
2019-12-19 17:35:31 +0000

    cmd args

--- main.py
+++ main.py
@@ -9,28 +9,31 @@ from error import oof
 import fn, math
 
 # Constants
-ops = [
-	{"ps": lambda x, y: x+y},
-	{"sb": lambda x, y: x-y},
-	{"mt": lambda x, y: x*y},
-	{"dv": lambda x, y: x/y},
-	{"pw": lambda x, y: x**y},
-	{"sq": lambda x: math.sqrt(x)},
-]
+ops = {
+	"ps": lambda x, y: x+y,
+	"sb": lambda x, y: x-y,
+	"mt": lambda x, y: x*y,
+	"dv": lambda x, y: x/y,
+	"pw": lambda x, y: x**y,
+	"sq": lambda x: math.sqrt(x),
+}
 keywords = [i for i in ops]
 
-cmds = [
-	{"echo": [lambda x: print(x), "Prints to terminal"]},
-	{"clear": [fn.cls, "Clears terminal"]},
-	{"script": [fn.runJuice, "Runs file"]},
-]
+cmds = {
+	"echo": [lambda x: print(x), "Prints to terminal"],
+	"clear": [fn.cls, "Clears terminal"],
+	"script": [fn.runJuice, "Runs file"],
+}
 cmdk = [i for i in cmds]
 
 print(colored("Welcome to SlurpTerminal"))
 while True:
 	print(">", end=" ")
-	cmd = input().lower().strip()
-	if not cmd in cmds:
+	cmdfull = input().lower().strip().split(" ")
+	cmd = cmdfull[0]
+	if not cmd in cmdk:
 		fn.out("Invalid Command! Use \"help\"", "red")
+	else:
+		cmdargs = " ".join(cmdfull[1:])
 	else:
 		cmds[cmd]
