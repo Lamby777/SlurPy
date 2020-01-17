326cde4d14763b0c3dd5a30de71fc3d1796765a2
Lamby777 <sheepareawesome777@gmail.com>
2020-01-17 13:37:05 +0000

    error comments

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ import fn, math
 
 # Error Classes
 
+# Top-Level Exceptions
 class MetaException(Exception): pass
 class JuiceFileException(Exception): pass
 class SlurpException(Exception): pass
+
+# Slurp Exceptions
 class SlurpSyntaxError(SlurpException): pass
 class SlurpMathError(SlurpException): pass
 
